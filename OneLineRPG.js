c2e9818250545e000cdc1fc3b618846e872bd75e
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-03-13 17:51:53 -0400

    Update OneLineRPG.js

--- OneLineRPG.js
+++ OneLineRPG.js
@@ -1,1 +1,1 @@
-
+let password_length = prompt("What should the length of the password be?"); for (let i = 0; i < password_length; i++) {let num = Math.floor(Math.random()*62); if (num >= 10) {if (num >= 36) {ID = ID + String.fromCharCode(num+29);} else {ID = ID + String.fromCharCode(num+87);}} else {ID = ID + num;}} console.log(ID);
